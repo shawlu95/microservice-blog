56b89ebe29bef5c4db013019d9d9e913c438c942
Shaw Lu <shawlu95@gmail.com>
2022-04-24 03:38:47 -0700

    React show comment status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,19 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.comment}</li>
+    var content;
+    switch (comment.status) {
+      case 'approved':
+        content = comment.comment;
+        break;
+      case 'rejected':
+        content = 'Removed by moderation';
+        break;
+      case 'pending':
+        content = 'Awaiting moderation';
+        break;
+    }
+    return <li key={comment.id}>{content}</li>
   })
 
   return <ul>{renderedComments}</ul>
