dd9a35e66c84d2a1b033c01699232e9bba546b4e
Shaw Lu <shawlu95@gmail.com>
2022-04-23 21:13:34 -0700

    Implement query service

--- query/index.js
+++ query/index.js
@@ -6,12 +6,22 @@ const app = express();
 app.use(parser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
-
+  const { type, data } = req.body;
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  } else if (type === 'CommentCreated') {
+    const { id, comment, postId } = data;
+    posts[postId].comments.push({ id, comment });
+  }
+  res.send({});
 });
 
 app.listen(4002, () => {
