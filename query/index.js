c5e305bf486b2895b13fdb9389b81052df694f26
Shaw Lu <shawlu95@gmail.com>
2022-04-24 04:08:58 -0700

    Sync query service when boot up

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const parser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -12,8 +13,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -26,9 +26,25 @@ app.post('/events', (req, res) => {
     const comment = post.comments.find(comment => comment.id === id);
     comment.status = status;
   }
+};
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+  try {
+    // pull all events from bus and catch up with the history
+    // if an event has already been handled, handle again as if new
+    const res = await axios.get("http://localhost:4005/events");
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
