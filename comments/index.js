236613214b76f202079125849bcd62d8a88527d8
Shaw Lu <shawlu95@gmail.com>
2022-04-23 19:14:48 -0700

    Create two node apps

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const parser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(parser.json());
+
+// store data in memory
+const comments = {};
+
+app.get('/post/:id/comment', (req, res) => {
+  const { id } = req.params;
+  res.send(comments[id]);
+});
+
+app.post('/post/:id/comment', (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+  if (!comments[id]) {
+    comments[id] = []
+  }
+  const commentId = randomBytes(4).toString('hex');
+  comments[id].push({ commentId, comment });
+  res.status(201).send(comments[id]);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on port 4001');
+});
---
+++ posts/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const parser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(parser.json());
+
+// store data in memory
+const posts = {};
+
+app.get('/post', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/post', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = { id, title };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
+});
